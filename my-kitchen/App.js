7b11043ae379aff69aac20ddaa89b9fd11f9dd72
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-03 00:52:54 +0300

    Added safe area view

--- my-kitchen/App.js
+++ my-kitchen/App.js
@@ -1,16 +1,21 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import LoginScreen from "./screens/LoginScreen";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Login" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={LoginScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "grey" }}>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="Login" screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Login" component={LoginScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
