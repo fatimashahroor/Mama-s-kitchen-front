4dde1eb327c79a3306a52fc1c8018a0dd038d091
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-04 00:35:09 +0300

    Styled the login button

--- my-kitchen/screens/LoginScreen/styles.js
+++ my-kitchen/screens/LoginScreen/styles.js
@@ -8,24 +8,84 @@ export default StyleSheet.create({
         },
     imageStyle: {
         width: "100%",
-        height: "54%",
+        height: 140,
         resizeMode: 'cover'
     },
     logoStyle: {
-        width: "23%",
-        height: "37%",
+        width: 90,
+        height: 95,
         borderRadius: 350,
         resizeMode: 'cover',
         position: 'absolute',
-        top: 80,
+        top: 90,
     },
     loginText: {
         fontSize: 40,
         color: '#B20530',
         textAlign: 'center',
-        marginTop: -30,
+        marginTop: 80,
     },
     customText: {
         fontFamily: 'Inter_600SemiBold',
-   }
+    },
+    customText1: {
+    fontFamily: 'Inter_400Regular',
+    },
+    textInput: {
+        height: 55,
+        borderColor: 'grey',
+        borderWidth: 1,
+        width: 280,
+        marginLeft: 50,
+        borderRadius: 30,
+        paddingLeft: 30,
+        fontSize: 18,
+    },
+    margin: {
+        marginTop: 40,
+    },
+    margin1: {
+        marginTop: 10,
+    },
+    forgotPass: {
+        textAlign: 'right',
+        marginRight: 52,
+        color: '#B20530',
+    },
+    button: {
+        backgroundColor: '#B20530',
+        width: 280,
+        height: 55,
+        borderRadius: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 32,
+        marginLeft: 50,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 19,
+        fontFamily: 'Inter_400Regular',
+    },
+    margin2: {
+        marginTop: 20,
+    },
+    accountText: {
+        textAlign: 'center',
+        color: 'black',
+    },
+    flexRow: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    signUp: {
+        color: '#B20530',
+        textDecorationLine: 'underline',
+    },
+    flexColumn: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
 })
