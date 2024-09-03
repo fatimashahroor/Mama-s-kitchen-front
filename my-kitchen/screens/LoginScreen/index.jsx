d5b899840afdc5887a5a5af75225fd1fbd79d9ec
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-03 11:44:59 +0300

    Added the logo image to login page

--- my-kitchen/screens/LoginScreen/index.jsx
+++ my-kitchen/screens/LoginScreen/index.jsx
@@ -4,7 +4,10 @@ import styles from "./styles";
 function LoginScreen() {
     return (
             <View>
-                <Image source={require('../../assets/plates1.jpg')} style={styles.imageStyle}/>
+                <View style={styles.container}> 
+                    <Image source={require('../../assets/plates1.jpg')} style={styles.imageStyle}/>
+                    <Image source={require('../../assets/logo.png')} style={styles.logoStyle}/>
+                </View>
             </View>
     );
 }
