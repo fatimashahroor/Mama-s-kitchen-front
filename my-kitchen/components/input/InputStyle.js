5b846d7c2f757a65a1e1c1d7c454b47786f643c0
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-04 00:35:41 +0300

    Styled the input component

---
+++ my-kitchen/components/input/InputStyle.js
@@ -0,0 +1,19 @@
+import { StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        marginVertical: -1,
+      },
+      input: {
+        height: 50,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginLeft: 50,
+        marginRight: 50,
+        borderRadius: 30,
+        fontSize: 18,
+        fontFamily: 'Inter_400Regular',
+        marginTop: 10,
+        paddingLeft: 20        
+      },
+})
--- my-kitchen/screens/LoginScreen/index.jsx
+++ my-kitchen/screens/LoginScreen/index.jsx
@@ -1,15 +1,17 @@
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, TouchableOpacity } from "react-native";
+import { useNavigation } from '@react-navigation/native';
 import styles from "./styles";
 import { useFonts, Inter_400Regular, Inter_700Bold, Inter_600SemiBold} from '@expo-google-fonts/inter';
+import InputText from "../../components/input/input";
+
 function LoginScreen() {
+    const navigation = useNavigation();  
     const [fontsLoaded] = useFonts({
         Inter_400Regular, Inter_700Bold, Inter_600SemiBold,
       });
-    
       if (!fontsLoaded) {
         return <View style={styles.container}><Text>Loading Fonts...</Text></View>;
       }
-
     return (
             <View>
                 <View style={styles.container}> 
@@ -17,6 +19,29 @@ function LoginScreen() {
                     <Image source={require('../../assets/logo.png')} style={styles.logoStyle}/>
                 </View>
                 <Text style={[styles.loginText, styles.customText]}>Login</Text>
+                <View style={styles.margin2}>
+                  <InputText placeholder="Email" placeholderTextColor={'#989997'}
+                            secureTextEntry={false}/>
+                  <InputText placeholder="Password" placeholderTextColor={'#989997'}
+                            secureTextEntry={true}/>
+                  <TouchableOpacity onPress={() => navigation.navigate('ForgotPasswordScreen')}>
+                    <Text style={[styles.customText1, styles.margin1, styles.forgotPass]}>Forgot Password?</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Login</Text>
+                  </TouchableOpacity>
+                  <View style={styles.flexRow}>
+                    <Text style={[styles.customText1, styles.margin2, styles.accountText]}>Don't have an account? </Text>
+                    <TouchableOpacity onPress={() => navigation.navigate('RegisterScreen')}>
+                      <Text style={[styles.customText1, styles.signUp, styles.margin2]}>Sign Up</Text></TouchableOpacity>
+                  </View>
+                  <View style={styles.flexColumn}>
+                    <Text style={[styles.customText1, styles.accountText, styles.margin]}>You're a cook and wants to {'\n'}start your
+                       journey with us? </Text>
+                    <TouchableOpacity onPress={() => navigation.navigate('ContactUs')}>
+                      <Text style={[styles.customText1, styles.signUp]}>Contact Us</Text></TouchableOpacity>
+                    </View>
+                </View>
             </View>
     );
 }
