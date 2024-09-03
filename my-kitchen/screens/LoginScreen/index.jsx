25de9ff672ad58b2ca971fdc01368e5a6fffa101
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-03 17:06:25 +0300

    Imported the fonts

--- my-kitchen/screens/LoginScreen/index.jsx
+++ my-kitchen/screens/LoginScreen/index.jsx
@@ -1,13 +1,22 @@
 import { Text, View, Image } from "react-native";
 import styles from "./styles";
-
+import { useFonts, Inter_400Regular, Inter_700Bold, Inter_600SemiBold} from '@expo-google-fonts/inter';
 function LoginScreen() {
+    const [fontsLoaded] = useFonts({
+        Inter_400Regular, Inter_700Bold, Inter_600SemiBold,
+      });
+    
+      if (!fontsLoaded) {
+        return <View style={styles.container}><Text>Loading Fonts...</Text></View>;
+      }
+
     return (
             <View>
                 <View style={styles.container}> 
                     <Image source={require('../../assets/plates1.jpg')} style={styles.imageStyle}/>
                     <Image source={require('../../assets/logo.png')} style={styles.logoStyle}/>
                 </View>
+                <Text style={[styles.loginText, styles.customText]}>Login</Text>
             </View>
     );
 }
