aaad3ec9390f5809e27eda628487e4bb6c51b986
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-06 23:43:08 +0300

    Edited space between dishes

--- my-kitchen/screens/ChefMenuScreen/styles.js
+++ my-kitchen/screens/ChefMenuScreen/styles.js
@@ -10,7 +10,7 @@ export default StyleSheet.create({
         height: 67,
         borderRadius: 35,
         marginTop: 40,
-        marginLeft: 5,
+        marginLeft: -2,
         marginRight: 8
     },
     icon: {
@@ -40,36 +40,84 @@ export default StyleSheet.create({
         marginHorizontal: 8,
         marginTop: 40,
         marginRight: 20
-      },
+    },
       horizontalLine: {
           width: "70%",
           height: 1.5,
           backgroundColor: '#FFD21C',
           marginTop: 20,
           marginLeft: 48
-      },
+    },
       chefPhone: {
         fontSize: 16,
         fontFamily: 'Inter_400Regular',
-      },
+    },
       chefAge: {
         fontSize: 16,
         fontFamily: 'Inter_400Regular',
         marginTop: 52,
         marginBottom: 8
-      },
+    },
       chefLocation: {
         fontSize: 16,
         fontFamily: 'Inter_400Regular',
-        marginLeft: 80,
+        marginLeft: 73,
         color: 'gray',
         marginTop: 5
-      },
+    },
       chefBio: {
         fontSize: 15,
         fontFamily: 'Inter_400Regular',
         color: 'black',
         marginTop: 20,
-        marginLeft: 10,
-      },
+        marginLeft: 2,
+    },
+      menuTitle: {
+        fontSize: 22,
+        fontFamily: 'Inter_600SemiBold',
+        marginTop: 10,
+        marginLeft: 2
+    },
+    image: {
+        width: 120,
+        height: 100,
+        marginLeft: 15,
+        marginRight: 30,
+        marginTop: 15,
+        borderRadius: 15,
+    },
+    dish: {
+        backgroundColor: "#FFD21C",
+        borderRadius: 30,
+        marginLeft: 2,
+        marginTop: 25,
+        width: 150,
+        height: 180,
+    },
+    dishName: {
+        marginTop: 7,
+        marginLeft: 35,
+        fontFamily: 'Inter_600SemiBold',
+        fontSize: 12,
+    },  
+    dishPrice: {
+        marginTop: 5,
+        marginRight: 15,
+        fontFamily: 'Inter_400Regular',
+        fontSize: 12,
+    },
+    space: {
+        alignContent: 'center',
+    },
+    cart: {
+        alignSelf: 'flex-end',
+        marginTop: -26,
+        marginRight: 15,  
+    },
+    dishesContainer: {
+        flexDirection: 'row',  
+        flexWrap: 'wrap',     
+        justifyContent: 'flex-start',
+        gap: 16
+    },
 })
