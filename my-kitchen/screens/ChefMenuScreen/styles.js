21f08eb600a8171ffd5590e7353a8fc745eb4c3a
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-06 20:32:24 +0300

    Adjusted style of image including top margin

--- my-kitchen/screens/ChefMenuScreen/styles.js
+++ my-kitchen/screens/ChefMenuScreen/styles.js
@@ -2,9 +2,14 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
     container: {
+        margin: 30,
+        flex: 1,
     },
     imageStyle: {
-        width: 45,
-        height: 45,
+        width: 67,
+        height: 67,
+        borderRadius: 35,
+        marginTop: 40,
+        marginLeft: 5,
     },
 })
