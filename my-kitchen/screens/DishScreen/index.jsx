991fa689f0648141bd3eb324976cf23ced8a35f4
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-07 17:56:46 +0300

    Imported necessary libraries

---
+++ my-kitchen/screens/DishScreen/index.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import { useFonts, Inter_400Regular, Inter_600SemiBold } from "@expo-google-fonts/inter";
+import { View, Text, StyleSheet, Image, ScrollView, TouchableOpacity, TextInput, Alert } from "react-native";
+import styles from "./styles";
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import { Ionicons } from '@expo/vector-icons';
+import { EXPO_PUBLIC_API_URL } from '@env';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const DishScreen = () => {}
+
+export default DishScreen;
