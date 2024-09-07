334bc1f08184b58a1ecb95fa1395f8d1a0663792
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-07 19:42:13 +0300

    Imported use navigation to navigate to other pages

--- my-kitchen/screens/DishScreen/index.jsx
+++ my-kitchen/screens/DishScreen/index.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useFonts, Inter_400Regular, Inter_600SemiBold } from "@expo-google-fonts/inter";
 import { View, Text, StyleSheet, Image, ScrollView, TouchableOpacity, TextInput, Alert } from "react-native";
 import styles from "./styles";
+import { useNavigation } from "@react-navigation/native";
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import { Ionicons } from '@expo/vector-icons';
 import { EXPO_PUBLIC_API_URL } from '@env';
@@ -10,7 +11,6 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 const DishScreen = () => {
     const [fontsLoaded] = useFonts({
         Inter_400Regular, Inter_600SemiBold});
-
     if (!fontsLoaded) {
         return <Text>Loading Fonts...</Text>;
     }
