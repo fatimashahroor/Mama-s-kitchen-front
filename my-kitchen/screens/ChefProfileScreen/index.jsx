cea92edf26d3b152c4d547714ebc4a15d24f3fd8
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-09 16:29:12 +0300

    Implemented avatar and camera icons

--- my-kitchen/screens/ChefProfileScreen/index.jsx
+++ my-kitchen/screens/ChefProfileScreen/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Text, View, Image, ScrollView, TouchableOpacity, TouchableWithoutFeedback } from "react-native";
 import { useFonts, Inter_400Regular, Inter_600SemiBold } from '@expo-google-fonts/inter';
+import { Ionicons } from '@expo/vector-icons';
 import { EXPO_PUBLIC_API_URL } from "@env";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import styles from "./styles";
@@ -46,6 +47,16 @@ const ChefProfileScreen = () => {
     return (
         <View style={styles.container}>
             <Text style={styles.title}>My Profile</Text>
+            <View style={styles.profileContainer}>
+                <View style={styles.avatarContainer}>
+                    <View style={styles.avatarCircle}>
+                        <Ionicons name="person-outline" size={50} color="gray" />
+                    </View>
+                    <TouchableOpacity style={styles.cameraIcon}>
+                        <Ionicons name="camera" size={24} color="#B20530" />
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
     );
 };
