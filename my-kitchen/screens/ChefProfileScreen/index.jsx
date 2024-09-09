bbfc1503f23213ce69e57ac3069f8bb1ad6534d2
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-09 15:51:13 +0300

    Loaded fonts

--- my-kitchen/screens/ChefProfileScreen/index.jsx
+++ my-kitchen/screens/ChefProfileScreen/index.jsx
@@ -1,12 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { Text, View, Image, ScrollView, TouchableOpacity, TouchableWithoutFeedback } from "react-native";
+import { useFonts, Inter_400Regular, Inter_600SemiBold } from '@expo-google-fonts/inter';
+import { EXPO_PUBLIC_API_URL } from "@env";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import ChefMenu from "../../components/menu";
 
-const ChefProfileScreen = () => {
+const ChefProfileScreen = ({route,navigation}) => {
+    const {chef} = route.params;
+    const [fontsLoaded] = useFonts({
+        Inter_400Regular, Inter_600SemiBold
+    });
     
+    if (!fontsLoaded) {
+        return <Text>Loading Fonts...</Text>;
+    }
+     
     return (
-        <View>
-            <Text>ChefProfileScreen</Text>
-        </View>
+        <ChefMenu 
+        chef={chef}
+        ChefMenu= {dish}
+        selectedDay={selectedDay}
+        handleDayPress={handleDayPress}/>
     );
 };
 
