40aec3131e0aa690a654b54974d77ba1ee64935f
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-10 14:21:43 +0300

    Imported the search input

--- my-kitchen/screens/EditDishesScreen/index.jsx
+++ my-kitchen/screens/EditDishesScreen/index.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, Image, TouchableOpacity, ScrollView, TouchableWithoutFeedback } from "react-native";
 import styles from "./styles";
+import SearchInput from "../../components/search/search";
 
 const EditDishesScreen = () => {
     return (
         <View style={styles.container}>
             <Text style={styles.text}>My Plates</Text>
+            <View>
+                <SearchInput placeholder=" Search for your dishes"/>
+            </View>
         </View>
     );
 };
