c4f2101566a580b02b7957c215e5d75d19d92275
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-10 14:02:53 +0300

    Declared the edit dishes component

---
+++ my-kitchen/screens/EditDishesScreen/index.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const EditDishesScreen = () => {
+    return (
+        <View>
+            <Text>EditDishesScreen</Text>
+        </View>
+    );
+};
+
+export default EditDishesScreen;
