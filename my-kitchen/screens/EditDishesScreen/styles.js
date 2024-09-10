7b250221a4cc70d5db11283d251b8be418325398
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-10 15:05:30 +0300

    Styled drop down options

--- my-kitchen/screens/EditDishesScreen/styles.js
+++ my-kitchen/screens/EditDishesScreen/styles.js
@@ -19,5 +19,24 @@ export default StyleSheet.create({
     menu: {
         marginTop: -23,
         marginLeft: 325,
-    }
+        color: '#B20530',
+    },
+    dropdownMenu: {
+        position: 'absolute',
+        top: 56, 
+        right: 37,
+        backgroundColor: 'white',
+        shadowColor: '#000',
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+        zIndex: 1, 
+    },
+    dropdownItem: {
+        padding: 10,
+        fontSize: 16,
+        width: 110,
+        fontFamily: 'Inter_400Regular',
+        marginLeft : 10,
+    },
 })
