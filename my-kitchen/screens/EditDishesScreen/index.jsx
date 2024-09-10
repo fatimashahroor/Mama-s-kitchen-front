e56a6390ee3b801570ff6e1c8e34f64ff883a061
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-10 15:10:54 +0300

    Fetched dishes of cook

--- my-kitchen/screens/EditDishesScreen/index.jsx
+++ my-kitchen/screens/EditDishesScreen/index.jsx
@@ -4,10 +4,41 @@ import styles from "./styles";
 import SearchInput from "../../components/search/search";
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import { Ionicons } from '@expo/vector-icons';
+import { EXPO_PUBLIC_API_URL } from '@env';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const EditDishesScreen = () => {
     const [menuVisible, setMenuVisible] = useState(false);
     const [searchQuery, setSearchQuery] = useState('');
+    const [dishes, setDishes] = useState([]);
+    const [error, setError] = useState(null);
+
+    const getDishes = async () => {
+        const chef = await AsyncStorage.getItem('user');
+        const chefId = JSON.parse(chef)._id;
+        try {
+            const response = await fetch(`${EXPO_PUBLIC_API_URL}/api/dishes/${chefId}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${await AsyncStorage.getItem('token')}`,
+                },
+            });
+            const data = await response.json();
+            if (response.ok) {
+                setDishes(data.data);
+            } else {
+                setError(data.message);
+            }
+        } catch (error) {
+            setError(error);
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getDishes();
+    }, []);
     return (
         <TouchableWithoutFeedback onPress={() => {setMenuVisible(false); {Keyboard.dismiss()}}}>
             <View style={styles.container}>
