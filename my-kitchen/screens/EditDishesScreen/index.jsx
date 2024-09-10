b0eabcb4a9d4645661bb544f5eb5aa56bbc8bd05
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-10 14:28:30 +0300

    Added menu icon

--- my-kitchen/screens/EditDishesScreen/index.jsx
+++ my-kitchen/screens/EditDishesScreen/index.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { View, Text, Image, TouchableOpacity, ScrollView, TouchableWithoutFeedback } from "react-native";
 import styles from "./styles";
 import SearchInput from "../../components/search/search";
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import { Ionicons } from '@expo/vector-icons';
 
 const EditDishesScreen = () => {
     return (
@@ -9,6 +11,7 @@ const EditDishesScreen = () => {
             <Text style={styles.text}>My Plates</Text>
             <View>
                 <SearchInput placeholder=" Search for your dishes"/>
+                <Ionicons style={styles.menu} name='menu' size={27}></Ionicons>
             </View>
         </View>
     );
