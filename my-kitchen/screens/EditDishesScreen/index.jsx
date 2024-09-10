cc258e11d26eca6ded34f701162c5a3316f6b21d
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-10 15:05:09 +0300

    Created drop down menu

--- my-kitchen/screens/EditDishesScreen/index.jsx
+++ my-kitchen/screens/EditDishesScreen/index.jsx
@@ -1,19 +1,32 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, Image, TouchableOpacity, ScrollView, TouchableWithoutFeedback } from "react-native";
+import { View, Text, Image, TouchableOpacity, ScrollView, TouchableWithoutFeedback, Keyboard } from "react-native";
 import styles from "./styles";
 import SearchInput from "../../components/search/search";
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import { Ionicons } from '@expo/vector-icons';
 
 const EditDishesScreen = () => {
+    const [menuVisible, setMenuVisible] = useState(false);
+    const [searchQuery, setSearchQuery] = useState('');
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>My Plates</Text>
-            <View>
-                <SearchInput placeholder=" Search for your dishes"/>
-                <Ionicons style={styles.menu} name='menu' size={27}></Ionicons>
+        <TouchableWithoutFeedback onPress={() => {setMenuVisible(false); {Keyboard.dismiss()}}}>
+            <View style={styles.container}>
+                <Text style={styles.text}>My Plates</Text>
+                <TouchableOpacity activeOpacity={1} >
+                    <SearchInput placeholder=" Search for your dishes"/>
+                    <TouchableOpacity onPress={() => setMenuVisible(!menuVisible)}>
+                        <Ionicons name='menu' size={27} style={styles.menu} />
+                    </TouchableOpacity>
+                    {menuVisible && (
+                        <View style={styles.dropdownMenu}>
+                            <Text style={styles.dropdownItem}>Create dish</Text>
+                            <Text style={styles.dropdownItem}>Edit dish</Text>
+                            <Text style={styles.dropdownItem}>Delete dish</Text>
+                        </View>
+                    )}
+                </TouchableOpacity>
             </View>
-        </View>
+        </TouchableWithoutFeedback>
     );
 };
 
