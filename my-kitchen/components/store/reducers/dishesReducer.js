943e6a9d123608b468db1ecd2f0085da61cf54e4
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-13 01:39:47 +0300

    Edited reducers

--- my-kitchen/components/store/reducers/dishesReducer.js
+++ my-kitchen/components/store/reducers/dishesReducer.js
@@ -10,9 +10,9 @@ const dishesReducer = function(state = initialState, action) {
         case "ADD_DISH":
             return {...state,  dishes: [...state.dishes, ...action.dish],  isLoading: true};
         case "DELETE_DISH":
-            return {...state,  dishes: state.dishes.filter(dish => dish.id !== action.dishId),  isLoading: true};
+            return {...state,  dishes: state.dishes.filter(dish => dish.id != action.dishId),  isLoading: true};
         case "UPDATE_DISH":
-            return {...state,  dishes: state.dishes.map(dish => dish.id === action.dish.id ? { ...dish, ...action.dish } : dish ),  isLoading: true};
+            return {...state,  dishes: state.dishes.map(dish => dish.id == action.dish.id ? { ...dish, ...action.dish } : dish ),  isLoading: true};
         default:
             return state;
     }
