f81deb481f7842fd83787d2dea427ce5cfa2d15f
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-12 06:24:09 +0300

    Created the dishReducer

---
+++ my-kitchen/components/store/reducers/dishesReducer.js
@@ -0,0 +1,22 @@
+const initialState = {
+    dishes: [],
+    isLoading: false
+};
+
+const dishesReducer = function(state = initialState, action) {
+    switch (action.type) {
+        case "GET_DISHES":
+            return {...state,  dishes: action.dishes,  isLoading: true};
+        case "ADD_DISH":
+            return {...state,  dishes: [...state.dishes, ...action.dish],  isLoading: true};
+        case "DELETE_DISH":
+            return {...state,  dishes: state.dishes.filter(dish => dish.id !== action.dishId),  isLoading: true};
+        case "UPDATE_DISH":
+            return {...state,  dishes: state.dishes.map(dish => dish.id === action.dish.id ? { ...dish, ...action.dish } : dish ),  isLoading: true};
+        default:
+            return state;
+    }
+}
+
+export default dishesReducer;
+
