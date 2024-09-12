34679d1e6c82ca0412d42ed6be8003d906ff6768
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-12 06:23:05 +0300

    Created the dish actions

---
+++ my-kitchen/components/store/actions/dishActions.js
@@ -0,0 +1,73 @@
+import {EXPO_PUBLIC_API_URL} from '@env';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const GET_DISHES = "GET_DISHES";
+export const ADD_DISH = "ADD_DISH";
+export const DELETE_DISH = "DELETE_DISH";
+export const UPDATE_DISH = "UPDATE_DISH";
+
+
+export const getDishes = () => async (dispatch) => {
+    const chef = await AsyncStorage.getItem('user');
+    const chefId = JSON.parse(chef).id;
+    try {
+        const response = await fetch(`${EXPO_PUBLIC_API_URL}/api/dishes/${chefId}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${await AsyncStorage.getItem('token')}`,
+            },
+        });
+        const data = await response.json();
+        if (response.ok) {
+            dispatch({type: GET_DISHES, dishes: data});
+        } else {
+            throw new Error(data.message);
+        }
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+export const addDish = (dish) => async (dispatch) => {
+    const chef = await AsyncStorage.getItem('user');
+    const chefId = JSON.parse(chef).id;
+    const form = new FormData();
+    form.append('chef_id', chefId);
+    form.append('name', dish.name);
+    form.append('main_ingredients', dish.main_ingredients);
+    form.append('steps', dish.steps);
+    form.append('price', dish.price);
+    form.append('available_on', dish.available_on);
+    form.append('diet_type', dish.diet_type);
+    form.append('additional_ings', dish.additional_ings);
+    form.append('duration', `${dish.duration.hours}:${dish.duration.minutes}:${dish.duration.seconds}`);
+    if (dish.currentImage) {
+        const uriParts = dish.currentImage.split('.');
+        const fileType = uriParts[uriParts.length - 1];
+        form.append('photo', {
+            uri: dish.currentImage,
+            name: `photo.${fileType}`, 
+            type: `image/${fileType}`
+        });
+    }
+    try {
+        const response = await fetch(`${EXPO_PUBLIC_API_URL}/api/dish/create`, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${await AsyncStorage.getItem('token')}`,
+            },
+            body: form
+        });
+        const newDish = await response.json();
+        if (response.ok) {
+            dispatch({type: ADD_DISH, dishes: [newDish]});
+        } else {
+            console.log(data.message);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}; 
+
+export const deleteDish = (dishId) => ({type: DELETE_DISH, dishId});
+export const updateDish = (dish) => ({type: UPDATE_DISH, dish});
