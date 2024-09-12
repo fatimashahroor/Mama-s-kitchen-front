4a624f5e07aa46c4c8b2619b4c38a421dbd90223
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-12 04:13:17 +0300

    Created a screen to add the additional ingredients

---
+++ my-kitchen/screens/AddIngredientsScreen/index.jsx
@@ -0,0 +1,159 @@
+import React, { useState, useEffect } from "react";
+import { View, Text, TouchableOpacity, Keyboard, TouchableWithoutFeedback, Modal, TextInput, ScrollView } from "react-native";
+import styles from "./styles";
+import SearchInput from "../../components/search/search";
+import { Ionicons } from '@expo/vector-icons';
+import { EXPO_PUBLIC_API_URL } from '@env';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const AddIngredientsScreen = () => {
+    const [searchQuery, setSearchQuery] = useState('');
+    const [error, setError] = useState(null);
+    const [ingredients, setIngredients] = useState([]);
+    const [ModalVisible, setModalVisible] = useState(false);
+    const [currentIngredient, setCurrentIngredient] = useState(null);
+    const getIngredients = async () => {
+        const chef = await AsyncStorage.getItem('user');
+        const chefId = JSON.parse(chef).id;
+        try{
+            const response = await fetch(`${EXPO_PUBLIC_API_URL}/api/additional_ing/${chefId}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${await AsyncStorage.getItem('token')}`,
+                },
+            });
+            const data = await response.json();
+            
+            if (response.ok) {
+                setIngredients(data);
+            } else {
+                setError(data.message);
+            }
+        } catch (error) {
+            setError(error);
+            console.error(error);
+        }
+    };
+
+    const createIngredient = async () => {
+        const chef = await AsyncStorage.getItem('user');
+        const chefId = JSON.parse(chef).id;
+        try {
+            const response = await fetch(`${EXPO_PUBLIC_API_URL}/api/additional_ing/create/${chefId}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${await AsyncStorage.getItem('token')}`,
+                },
+                body: JSON.stringify({name: currentIngredient.name, cost: currentIngredient.cost})
+            });
+            const data = await response.json();
+            if (!response.ok) {
+                throw new Error("Failed to create ingredient");
+            }
+            getIngredients();
+        } catch (error) {
+            setError(error);
+        }
+    }
+
+    const deleteIngredient = async () => {
+        try {
+            const response = await fetch(`${EXPO_PUBLIC_API_URL}/api/additional_ing/delete/${currentIngredient.id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${await AsyncStorage.getItem('token')}`,
+                }
+            });
+            if (!response.ok) {
+                throw new Error("Failed to delete ingredient");
+            }
+            getIngredients();
+        } catch (error) {
+            setError(error);
+        }
+    }
+
+    const updateIngredient = async () => {
+        try {
+            const response = await fetch(`${EXPO_PUBLIC_API_URL}/api/additional_ing/update/${currentIngredient.id}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${await AsyncStorage.getItem('token')}`,
+                },
+                body: JSON.stringify({name: currentIngredient.name, cost: currentIngredient.cost})
+            });
+            const data = await response.json();
+            console.log(response);
+            if (!response.ok) {
+                throw new Error("Failed to update ingredient");
+            }
+            getIngredients();
+        } catch (error) {
+            setError(error);
+        }
+    }
+    useEffect(() => {
+        getIngredients();
+    }, []);
+    const filteredIngredients = ingredients.filter(ingredient => ingredient.name.toLowerCase().includes(searchQuery.toLowerCase()));
+    return (
+        <TouchableWithoutFeedback onPress={() => {{Keyboard.dismiss()}}}>
+            <View style={styles.container}>
+                <Text style={styles.text}>My Ingredients</Text>
+                <TouchableOpacity activeOpacity={1} >
+                    <SearchInput placeholder="Search for your ingredients" value={searchQuery} onChangeText={setSearchQuery}/>
+                    <TouchableOpacity onPress={() => {setModalVisible(true); setCurrentIngredient({name : '', cost: ''})}}>
+                        <Ionicons name='add-circle-outline' size={27} style={styles.menu} />
+                    </TouchableOpacity>
+                </TouchableOpacity>
+                <View style={styles.scrollView}>
+                    <ScrollView vertical={true} showsVerticalScrollIndicator={true}>
+                        <View style={styles.IngContainer}>
+                        {filteredIngredients.length > 0 ? filteredIngredients.map((item, index)=> (
+                                    <View key={item.id || index} style={styles.ing}>
+                                        <View style={[styles.flexRow, styles.space]}>
+                                            <TextInput style={styles.name}>{item.name}</TextInput>
+                                            <TextInput style={styles.cost}>{item.cost + "$"}</TextInput>
+                                            <TouchableOpacity onPress={() => {setModalVisible(true); setCurrentIngredient(item)}}>
+                                                <Ionicons style={styles.editButton} name="create-outline" size={20}/>
+                                            </TouchableOpacity>
+                                            <TouchableOpacity onPress={() => {setModalVisible(false); setCurrentIngredient(item); deleteIngredient()}}>
+                                                <Ionicons style={styles.trash} name="trash" size={20}/>
+                                            </TouchableOpacity>
+                                        </View>
+                                    </View> 
+                                )) : <Text style={styles.none}>No additional ingredients found</Text>}
+                        </View>
+                    </ScrollView>
+                </View>
+                <Modal
+                    animationType="slide" transparent={true} visible={ModalVisible}
+                    onRequestClose={() => {setModalVisible(!ModalVisible); setCurrentIngredient(null)}}>
+                    <View style={styles.centeredView}>
+                        <View style={styles.modalView}>
+                            <TextInput style={styles.modalText} placeholder="Ingredient name" value={ currentIngredient ? String(currentIngredient.name) : ''}
+                            onChangeText={text => setCurrentIngredient({...currentIngredient, name: text})}/>
+                            <TextInput style={styles.modalText} placeholder="cost" value={ currentIngredient ? String(currentIngredient.cost) : ''}
+                            onChangeText={text => setCurrentIngredient({...currentIngredient, cost: text})}/>
+                            <View style={styles.flexRow}>
+                                <TouchableOpacity
+                                    style={[styles.button, styles.buttonClose]} onPress={() => setModalVisible(!ModalVisible)}>
+                                    <Text style={styles.textStyle}>Cancel</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity style={[styles.button, styles.buttonClose]} onPress={() => {
+                                    {if (currentIngredient && currentIngredient.id) {updateIngredient();} else {createIngredient();}  setModalVisible(false);} }}>
+                                    <Text style={styles.textStyle}>Confirm</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </View>
+                </Modal>
+            </View>
+        </TouchableWithoutFeedback>
+    );
+};
+export default AddIngredientsScreen;
---
+++ my-kitchen/screens/AddIngredientsScreen/styles.js
@@ -0,0 +1,112 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    text: {
+        fontSize: 28,
+        textAlign: 'center',
+        marginTop: 50,
+        marginBottom: 10,
+        fontFamily: 'Inter_400Regular',
+    },
+    menu: {
+        marginTop: -23,
+        marginLeft: 329,
+        color: '#B20530',
+    },
+    flexColumn: {
+        display: 'flex',
+        flexDirection: 'column',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    flexRow: {
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    },
+    scrollView: {
+        flex: 2,
+        height: "100%",
+        marginTop: 20,
+        marginRight: 2,
+    },
+    space: {
+        justifyContent: 'center',
+    },
+    none: {
+        textAlign: 'center',
+        marginTop: 20,
+        fontSize: 20,
+        color: '#B20530',
+    },
+    IngContainer: {
+        display: 'flex',
+        padding: 10,
+        marginLeft: 18,
+        marginRight: 20,
+    },
+    ing: {
+        paddingVertical: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc', 
+    },
+    name: {
+        fontSize: 16,
+        flex: 1,
+        fontFamily: 'Inter_600SemiBold',
+    },
+    cost: {
+        fontSize: 16,
+        flex: 1,
+        fontFamily: 'Inter_400Regular',
+    },
+    editButton: {
+        marginRight: 50,
+    },
+    trash: {
+        flex: 1,
+    },
+    centeredView: {
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 270
+    },
+    modalView: {
+        backgroundColor: "#fff",
+        borderRadius: 30,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 50,
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: "flex-start",
+        width: 240,
+        borderWidth: 1,
+        borderColor: '#B20530',
+        paddingLeft: 10,
+        paddingBottom: 5
+    },
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        margin: 5,
+        width: 100,
+    },
+    buttonClose: {
+        borderWidth: 1,
+        borderColor: '#B20530',
+    },
+    textStyle: {
+        color: "black",
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+})
