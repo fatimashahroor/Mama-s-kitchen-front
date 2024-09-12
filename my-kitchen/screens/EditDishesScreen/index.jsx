767b2547931c901bec70d41152f6538dc5712d12
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-12 06:21:15 +0300

    Imported the get amd create dish actions

--- my-kitchen/screens/EditDishesScreen/index.jsx
+++ my-kitchen/screens/EditDishesScreen/index.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addDish, getDishes} from "../../components/store/actions/dishActions";
 import { View, Text, Image, TouchableOpacity, ScrollView, TouchableWithoutFeedback, Keyboard, Modal, TextInput } from "react-native";
 import styles from "./styles";
 import SearchInput from "../../components/search/search";
@@ -8,39 +10,18 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import {Picker} from '@react-native-picker/picker';
 import * as ImagePicker from 'expo-image-picker';
 const EditDishesScreen = () => {
+    const dispatch = useDispatch();
     const [searchQuery, setSearchQuery] = useState('');
-    const [dishes, setDishes] = useState([]);
+    const { dishes } = useSelector(state => state.dishes);
     const [error, setError] = useState(null);
     const [ModalVisible, setModalVisible] = useState(false);
-    const [currentDish, setCurrentDish] = useState(null);
-    const [availableOn, setAvailableOn] = useState('');
-    const [dietType, setDietType] = useState('');
-    const [currentImage, setCurrentImage] = useState(null);
-    const [duration, setDuration] = useState({
-        hours: '', minutes: '', seconds: ''});
-    const getDishes = async () => {
-        const chef = await AsyncStorage.getItem('user');
-        const chefId = JSON.parse(chef).id;
-        try {
-            const response = await fetch(`${EXPO_PUBLIC_API_URL}/api/dishes/${chefId}`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${await AsyncStorage.getItem('token')}`,
-                },
-            });
-            const data = await response.json();
-            if (response.ok) {
-                setDishes(data);
-            } else {
-                setError(data.message);
-            }
-        } catch (error) {
-            setError(error);
-            console.error(error);
-        }
-    };
+    const [currentDish, setCurrentDish] = useState({
+        name: '', main_ingredients: '', steps: '', price: '', available_on: 'Daily', diet_type: '', duration: { hours: '00', minutes: '30', seconds: '00' }, 
+        currentImage: null, diet_type: '', additional_ings: []});
 
+    useEffect(() => {
+        dispatch(getDishes());
+    }, [dispatch]);
     const deleteDish = async () => {
         console.log("currentDish.id", currentDish.id);
         try {
@@ -55,13 +36,11 @@ const EditDishesScreen = () => {
                 throw new Error("Failed to delete dish");
             }
             setDishes(prevDishes => prevDishes.filter(dish => dish.id !== currentDish.id));
-            getDishes();
         } catch (error) {
             setError(error);
             console.error(error);
         }
-    }
-
+    };
     const handleDelete = (item) => {
         deleteDish(item);
     };
@@ -100,8 +79,13 @@ const EditDishesScreen = () => {
             }
         } catch (error) {
             setError(error);
-            getDishes();
         }
+    };
+    const updateDuration = (field, value) => {
+        setCurrentDish(prev => ({
+            ...prev,
+            duration: { ...prev.duration, [field]: value }
+        }));
     };
     const pickImage = async () => {
         const result = await ImagePicker.launchImageLibraryAsync({
@@ -109,78 +93,21 @@ const EditDishesScreen = () => {
             allowsEditing: true, aspect: [4, 3], quality: 1,
         });
         if (!result.canceled) {
-            setCurrentImage(result.assets[0].uri);
+            setCurrentImage(prev => ({...prev, currentImage: result.assets[0].uri}));
         }
     };
-    const createDish = async () => {
-        const chef = await AsyncStorage.getItem('user');
-        const chefId = JSON.parse(chef).id;
-        const form = new FormData();
-        form.append('user_id', chefId);
-        form.append('name', currentDish.name);
-        form.append('main_ingredients', currentDish.main_ingredients);
-        form.append('steps', currentDish.steps);
-        form.append('price', currentDish.price);
-        form.append('available_on', availableOn);
-        form.append('diet_type', dietType);
-        form.append('duration', `${duration.hours}:${duration.minutes}:${duration.seconds}`);  
-        if (currentImage) {
-            const uriParts = currentImage.split('.');
-            const fileType = uriParts.pop();
-            form.append('photo', {
-                uri: currentImage,
-                name: `photo.${fileType}`,
-                type: `image/${fileType}`
-            });
-        }
-        try {
-            const response = await fetch(`${EXPO_PUBLIC_API_URL}/api/dish/create`, {
-                method: 'POST',
-                headers: {
-                    'Authorization': `Bearer ${await AsyncStorage.getItem('token')}`,
-                },
-                body: form
-            })
-            const data = await response.json();
-            console.log('data', data);
-            const dish = await data;
-            if (dish) {
-                getDishes(data);
-            }
-        } catch (error) {
-            setError(error);
-            getDishes();
-        }
-    }
     const filteredDishes = dishes.filter(dish => dish.name.toLowerCase().includes(searchQuery.toLowerCase()));
-    useEffect(() => {
-        getDishes();
-    }, []);
-    useEffect(() => {
-        if (currentDish) {
-            setAvailableOn(currentDish.available_on || '');
-            setDietType(currentDish.diet_type || '');
-            if (currentDish.duration) {
-                const parts = currentDish.duration.split(':');
-                setDuration({
-                    hours: parts[0],
-                    minutes: parts[1],
-                    seconds: parts[2]
-                });
-            }
-            if (currentDish.image_path)
-                setCurrentImage(`${EXPO_PUBLIC_API_URL}/images/${currentDish.image_path}`);
-        }
-    }, [currentDish]);    
-    const updateDuration = (value, field) => {
-        if (!isNaN(value) && value >= 0) {
-            if ((field === 'minutes' || field === 'seconds') && value < 60) {
-                setDuration({ ...duration, [field]: value });
-            } else if (field === 'hours') {
-                setDuration({ ...duration, [field]: value });
-            }
-        }
+    const handleInputChange = (field, value) => {
+        setCurrentDish(prev => ({ ...prev, [field]: value }));
     };
+    const handleConfirm = () => { if (currentDish.id) { updateDish(currentDish);
+        } else  dispatch(addDish(currentDish));
+        setModalVisible(false); setCurrentDish({id: null, name: '', main_ingredients: '', steps: '', price: '', available_on: 'Daily',
+            diet_type: '', duration: { hours: '00', minutes: '30', seconds: '00' }, currentImage: null, additional_ings: []});};
+
+    const handleCancel = () => {
+        setModalVisible(false); setCurrentDish({id: null, name: '', main_ingredients: '', steps: '', price: '', available_on: 'Daily',
+            diet_type: '', duration: { hours: '00', minutes: '30', seconds: '00' }, currentImage: null, additional_ings: []});};
     return (
         <TouchableWithoutFeedback onPress={() => {{Keyboard.dismiss()}}}>
             <View style={styles.container}>
@@ -188,8 +115,7 @@ const EditDishesScreen = () => {
                 <TouchableOpacity activeOpacity={1} >
                     <SearchInput placeholder=" Search for your dishes" value={searchQuery} onChangeText={setSearchQuery}/>
                     <TouchableOpacity onPress={() => { setCurrentDish({name: '', main_ingredients: '', steps: '', price: '', image_path: '', diet_type: '', 
-                                available_on: 'Daily'}); setModalVisible(true); setAvailableOn('Daily'); setCurrentImage(null); 
-                                setModalVisible(true); setDuration({ hours: '00', minutes: '00', seconds: '00' });}}>
+                                available_on: 'Daily', additional_ings: [], duration: '00:30:00'}); setModalVisible(true);}}>
                         <Ionicons name='add-circle-outline' size={27} style={styles.menu} />
                     </TouchableOpacity>
                 </TouchableOpacity>
@@ -218,12 +144,11 @@ const EditDishesScreen = () => {
                 </View>
                 <Modal
                     animationType="slide" transparent={true} visible={ModalVisible}
-                    onRequestClose={() => {setModalVisible(!ModalVisible); setCurrentDish(null); setDuration({ hours: '', minutes: '', seconds: '' });
-                    setAvailableOn(''); setDietType(''); }}>
+                    onRequestClose={() => {setModalVisible(!ModalVisible); setCurrentDish(null);}}>
                     <View style={styles.centeredView}>
                         <View style={styles.modalView}>
-                            <TextInput style={styles.modalText} placeholder="Plate's Name" value={currentDish ? currentDish.name : ''}
-                                onChangeText={(text) => setCurrentDish({...currentDish, name: text})}/>
+                            <TextInput style={styles.modalText} placeholder="Plate's Name" value={currentDish.name}
+                                onChangeText={(text) => handleInputChange('name', text)}/>
                             <TextInput style={styles.modalText} placeholder="Price" value={currentDish ? String(currentDish.price) : ''}
                                 onChangeText={(text) => setCurrentDish({...currentDish, price: text})}/>
                             <TextInput style={styles.modalText} placeholder="Main ingredients" value={currentDish ? String(currentDish.main_ingredients) : ''}
@@ -234,23 +159,17 @@ const EditDishesScreen = () => {
                                 <Text style={styles.text1}>Duration:</Text>
                                 <TextInput
                                     style={styles.durationInput} placeholder="hrs" keyboardType="numeric"
-                                    value={duration.hours}
-                                    onChangeText={(text) => updateDuration(text, 'hours')}
-                                />
+                                    value={currentDish.duration.hours}  onChangeText={(text) => updateDuration(text, 'hours')}/>
                                 <TextInput
                                     style={styles.durationInput} placeholder="mins" keyboardType="numeric"
-                                    value={duration.minutes}
-                                    onChangeText={(text) => updateDuration(text, 'minutes')}
-                                />
+                                    value={currentDish.duration.minutes} onChangeText={(text) => updateDuration(text, 'minutes')}/>
                                 <TextInput
                                     style={styles.durationInput} placeholder="secs" keyboardType="numeric"
-                                    value={duration.seconds}
-                                    onChangeText={(text) => updateDuration(text, 'seconds')}
-                                />
+                                    value={currentDish.duration.seconds} onChangeText={(text) => updateDuration(text, 'seconds')}/>
                             </View>
                             <View style={styles.pickerContainer}>
                                 <Picker
-                                    selectedValue={dietType} onValueChange={(itemValue) => setDietType(itemValue)}
+                                    selectedValue={currentDish.diet_type} onValueChange={(itemValue) => setDietType(itemValue)}
                                     style={[styles.picker]}>
                                     <Picker.Item label="Vegan" value="Vegan" />
                                     <Picker.Item label="Veggie" value="Veggie" />
@@ -260,7 +179,7 @@ const EditDishesScreen = () => {
                             </View>
                             <View style={[styles.pickerContainer, styles.marginTop]}>
                                 <Picker
-                                    selectedValue={availableOn} onValueChange={(itemValue) => setAvailableOn(itemValue)}
+                                    selectedValue={currentDish.available_on} onValueChange={(itemValue) => setAvailableOn(itemValue)}
                                     style={[styles.picker]}>
                                     <Picker.Item label="Daily" value="Daily" />
                                     <Picker.Item label="Monday" value="Monday" />
@@ -272,17 +191,25 @@ const EditDishesScreen = () => {
                                     <Picker.Item label="Sunday" value="Sunday" />
                                 </Picker>
                             </View>
-                            <Image source={{ uri: currentImage }} style={styles.modalImage} />
+                            {/* <View style={[styles.pickerContainer, styles.marginTop]}>
+                                <Picker
+                                    selectedValue={currentDish.additional_ings} onValueChange={(itemValue) => setAdditionalIngs(itemValue)}
+                                    style={[styles.picker]}>
+                                    {AdditionalIngs.map((ingredient) => (
+                                        <Picker.Item key={ingredient} label={ingredient.name} value={ingredient.id}/>
+                                    ))}
+                                </Picker>
+                            </View> */}
+                            <Image source={{ uri: currentDish.currentImage }} style={styles.modalImage} />
                             <TouchableOpacity onPress={pickImage} style={styles.imagePickerIcon}>
                                 <Ionicons name="camera" size={24} color="#B20530" />
                             </TouchableOpacity>
                             <View style={styles.flexRow}>
                                 <TouchableOpacity
-                                    style={[styles.button, styles.buttonClose]} onPress={() => setModalVisible(!ModalVisible)}>
+                                    style={[styles.button, styles.buttonClose]} onPress={() => handleCancel()}>
                                     <Text style={styles.textStyle}>Cancel</Text>
                                 </TouchableOpacity>
-                                <TouchableOpacity style={[styles.button, styles.buttonClose]} onPress={() => 
-                                    {if (currentDish && currentDish.id) {updateDish();} else {createDish();}  setModalVisible(false);}}>
+                                <TouchableOpacity style={[styles.button, styles.buttonClose]} onPress={() => handleConfirm()}>
                                     <Text style={styles.textStyle}>Confirm</Text>
                                 </TouchableOpacity>
                             </View>
