155a50fc7e8515fa516578c8a31e76c68614ebbb
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-20 21:29:57 +0300

    Added ion icons

--- my-kitchen/screens/RegisterScreen/index.jsx
+++ my-kitchen/screens/RegisterScreen/index.jsx
@@ -4,7 +4,6 @@ import { useNavigation } from '@react-navigation/native';
 import styles from "./styles";
 import { useFonts, Inter_400Regular,Inter_600SemiBold} from '@expo-google-fonts/inter';
 import InputText from "../../components/input/input";
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import { Ionicons } from '@expo/vector-icons';
 import { EXPO_PUBLIC_API_URL } from '@env';
 import AsyncStorage from '@react-native-async-storage/async-storage';
