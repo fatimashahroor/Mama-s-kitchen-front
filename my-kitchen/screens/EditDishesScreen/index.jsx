ce5249cae5abeb477f4a87757f852b97c469fe4a
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-09-23 16:38:06 +0300

    Fixed bugs related to updating dish

--- my-kitchen/screens/EditDishesScreen/index.jsx
+++ my-kitchen/screens/EditDishesScreen/index.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addDish, getDishes, updateDish , deleteDish} from "../../components/store/actions/dishActions";
-import { getIngredients } from "../../components/store/actions/ingredientsActions";
+import { addDish, initiateDishes, updateDish , removeDish} from "../../utils/redux/dishesSlice";
 import { View, Text, Image, TouchableOpacity, ScrollView, TouchableWithoutFeedback, Keyboard, Modal, TextInput } from "react-native";
 import styles from "./styles";
 import SearchInput from "../../components/search/search";
@@ -9,24 +8,156 @@ import { Ionicons } from '@expo/vector-icons';
 import {Picker} from '@react-native-picker/picker';
 import * as ImagePicker from 'expo-image-picker';
 import { EXPO_PUBLIC_API_URL } from '@env';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 const EditDishesScreen = () => {
     const dispatch = useDispatch();
     const [searchQuery, setSearchQuery] = useState('');
-    let { dishes } = useSelector(state => state.dishes);
-    let {ingredients} = useSelector(state => state.ingredients);
+    const dishes = useSelector(state => state.dishes.dishesList);
+    const [ingredients, setIngredients] = useState([]); 
     const [ModalVisible, setModalVisible] = useState(false);
     const [currentDish, setCurrentDish] = useState({
         name: '', main_ingredients: '', steps: '', price: '', available_on: 'Daily', diet_type: '', duration: { hours: '00', minutes: '00', seconds: '00' }, 
         currentImage: '', diet_type: '', selectedIngredients: []});
-    useEffect(() => {
-        dispatch(getDishes());
-    }, []);
+
+    const getDishes = async () => {
+        const chef = await AsyncStorage.getItem('user');
+        const chefId = JSON.parse(chef).id;
+        try {
+            const response = await fetch(`${EXPO_PUBLIC_API_URL}/api/dishes/${chefId}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${await AsyncStorage.getItem('token')}`,
+                },
+            });
+            const data = await response.json();
+            dispatch(initiateDishes(data));
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+
+    const createDish = async (dish) => {
+        const chef = await AsyncStorage.getItem('user');
+        const chefId = JSON.parse(chef).id;
+        let additionalIngsString = dish.additional_ings.join(',');
+        let form = new FormData();
+        form.append('user_id', chefId);
+        form.append('name', dish.name);
+        form.append('main_ingredients', dish.main_ingredients);
+        form.append('steps', dish.steps);
+        form.append('price', parseInt(dish.price));
+        form.append('available_on', dish.available_on);
+        form.append('diet_type', dish.diet_type);
+        form.append(`additional_ings`, additionalIngsString);
+        form.append('duration', dish.duration);
+        if (dish.currentImage) {
+            const uriParts = dish.currentImage.split('.');
+            const fileType = uriParts[uriParts.length - 1];
+            form.append('photo', {
+                uri: dish.currentImage,
+                name: `image.${fileType}`,
+                type: `image/${fileType}`
+            });
+        }
+        try {
+            const token = await AsyncStorage.getItem('token');
+            const response = await fetch(`${EXPO_PUBLIC_API_URL}/api/dish/create`, {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Accept': 'application/json',
+                },
+                body: form
+            });
+            dispatch(addDish( await response.json()));
+        } catch (error) {
+            console.log("Error parsing JSON or network error", error);
+            getDishes();
+        }
+    };
+
+    const editDish = async (dish) => {
+        let form1 = new FormData();
+        form1.append('name', dish.name);
+        form1.append('main_ingredients', dish.main_ingredients);
+        form1.append('steps', dish.steps);
+        form1.append('price', parseInt(dish.price));
+        form1.append('available_on', dish.available_on);
+        form1.append('diet_type', dish.diet_type);
+        form1.append('duration', dish.duration);
+        if (dish.currentImage !== dishes.find( d => d.id === dish.id).image_path) {
+            const uriParts = dish.currentImage.split('.');
+            const fileType = uriParts[uriParts.length - 1];
+            form1.append('photo', {
+                uri: dish.currentImage,
+                name: `image.${fileType}`, 
+                type: `image/${fileType}`
+            });
+        }
+        try {
+            const response = await fetch(`${EXPO_PUBLIC_API_URL}/api/dish/update/${dish.id}`, {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${await AsyncStorage.getItem('token')}`,
+                    'Accept': 'application/json',
+                },
+                body: form1
+            });
+            const data = await response.json();
+            if (response.ok) {
+                dispatch(updateDish(data));
+                getDishes();
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const deleteDish = async (dishId) => {
+        try {
+            const response = await fetch(`${EXPO_PUBLIC_API_URL}/api/dish/delete/${dishId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${await AsyncStorage.getItem('token')}`,
+                },
+            });
+            if (!response.ok) {
+                throw new Error("Failed to delete dish");
+            }
+            const data = await response.json();
+            dispatch(removeDish(dishId));
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const getIngredients = async () => {
+        const chef = await AsyncStorage.getItem('user');
+        const chefId = JSON.parse(chef).id;
+        try {
+            const response = await fetch(`${EXPO_PUBLIC_API_URL}/api/additional_ing/${chefId}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${await AsyncStorage.getItem('token')}`,
+                },
+            });
+            const data = await response.json();
+            setIngredients(data);
+        } catch (error) {
+            throw new Error(error);
+        }
+    };
 
     useEffect(() => {
-        dispatch(getIngredients());
+        getDishes();
+        getIngredients();
     }, []);
     const handleDelete = (dishId) => {
-        dispatch(deleteDish(dishId));};
+        deleteDish(dishId);
+    };
 
     const updateDuration = (field, value) => {
         setCurrentDish(dish => ({
@@ -61,22 +192,21 @@ const EditDishesScreen = () => {
             setCurrentDish(dish => ({ ...dish, currentImage: result.assets[0].uri }));
         }
     };
-    const filteredDishes = dishes.filter(dish => dish.name.toLowerCase().includes(searchQuery.toLowerCase()));
+    const filteredDishes = dishes.filter(dish => dish.name &&  dish.name.toLowerCase().includes(searchQuery.toLowerCase()));
     const handleInputChange = (field, value) => {
         setCurrentDish(prev => ({ ...prev, [field]: value }));
     };
     const handleConfirm = () => { if (currentDish.id) {
-        dispatch(updateDish({
+        editDish({
             ...currentDish,
-            duration: `${currentDish.duration.hours}:${currentDish.duration.minutes}:${currentDish.duration.seconds}`,
-            additional_ings: currentDish.selectedIngredients
-        }));
+            duration: `${currentDish.duration.hours}:${currentDish.duration.minutes}:${currentDish.duration.seconds}`
+        });
     } else {
-        dispatch(addDish({
+        createDish({
             ...currentDish,
             duration: `${currentDish.duration.hours}:${currentDish.duration.minutes}:${currentDish.duration.seconds}`,
             additional_ings: currentDish.selectedIngredients
-        }));
+        });
     }; setModalVisible(false); 
         setCurrentDish({id: null, name: '', main_ingredients: '', steps: '', price: '', available_on: 'Daily', diet_type: '', 
             duration: { hours: '00', minutes: '00', seconds: '00' }, currentImage: '', selectedIngredients: []});};
@@ -88,7 +218,8 @@ const EditDishesScreen = () => {
             <View style={styles.container}>
                 <Text style={styles.text}>My Plates</Text>
                 <TouchableOpacity activeOpacity={1} >
-                    <SearchInput placeholder=" Search for your dishes" value={searchQuery} onChangeText={setSearchQuery}/>
+                    <SearchInput placeholder=" Search for your dishes" value={searchQuery} onChangeText={setSearchQuery}
+                        width={'72%'}/>
                     <TouchableOpacity onPress={() => { setCurrentDish({name: '', main_ingredients: '', steps: '', price: '', currentImage: '', diet_type: '', 
                                 available_on: 'Daily', selectedIngredients: [], duration: { hours: '00', minutes: '00', seconds: '00' }}); setModalVisible(true);}}>
                         <Ionicons name='add-circle-outline' size={27} style={styles.menu} />
@@ -168,7 +299,8 @@ const EditDishesScreen = () => {
                                     <Picker.Item label="Sunday" value="Sunday" />
                                 </Picker>
                             </View>
-                            <View style={[styles.checkbox, styles.marginTop]}>
+                            {!currentDish.id ? (
+                                <View style={[styles.checkbox, styles.marginTop]}>
                                 <Text style={styles.checkboxLabel}>Choose additional ingredients:</Text>
                                 {ingredients.length > 0 ? (ingredients.map(ingredient => (
                                         <TouchableOpacity key={ingredient.id} style={styles.checkboxContainer}
@@ -181,8 +313,21 @@ const EditDishesScreen = () => {
                                 ) : (
                                     <Text>No ingredients found</Text>
                                 )}
-                            </View>
-                            <Image source={{ uri: `${EXPO_PUBLIC_API_URL}/images/${currentDish.currentImage}` }} style={styles.modalImage} />
+                                </View>   
+                            ): null}
+                            {currentDish.id && currentDish.currentImage === dishes.find( d => d.id === currentDish.id).image_path? (
+                            <Image
+                                source={{ uri: `${EXPO_PUBLIC_API_URL}/images/${currentDish.currentImage}` }}
+                                style={styles.modalImage}
+                            />
+                            ) : currentDish.currentImage ? (
+                            <Image
+                                source={{ uri: `${currentDish.currentImage}` }}
+                                style={styles.modalImage}
+                            />
+                            ) : (
+                            <View style={{ width: '100%', height: 100, backgroundColor: 'white' }} />
+                            )}
                             <TouchableOpacity onPress={pickImage} style={styles.imagePickerIcon}>
                                 <Ionicons name="camera" size={24} color="#B20530" />
                             </TouchableOpacity>
